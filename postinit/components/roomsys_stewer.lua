07d2ea5cfb32501e56bc65a01e459bca122f7ebb
lizzary <2962354027@qq.com>
2026-01-19 19:33:18 +0800

    v1.1.3 -优化产出厨房加成料理的流程，提高兼容性

--- postinit/components/roomsys_stewer.lua
+++ postinit/components/roomsys_stewer.lua
@@ -2,88 +2,87 @@ local AddComponentPostInit = AddComponentPostInit
 GLOBAL.setfenv(1, GLOBAL)
 local cooking = require("cooking")
 
-
-local function roomsysHarvest(self,harvester)
-    if self.done then
-        if self.onharvest ~= nil then
-            self.onharvest(self.inst)
-        end
-
-        if self.product ~= nil then
-            local loot
-
-            --有标签，直接换生成物
-            if self.inst:HasTag("in_kitchen") then
-                --便携香料站不与厨房系统互动，但是带有厨房加成的料理加香料能产出带有厨房加成和香料的料理，具体参考kitchen_buff_foods.lua
-                if self.inst:HasTag("spicer") then loot = SpawnPrefab(self.product) else loot = SpawnPrefab(self.product .. "_kitchen_buff") end
-
-                --如果是一些不可食用或者合成表没的料理就返回原料理
-                if not loot then loot = SpawnPrefab(self.product) end
-
-            else--没标签，判断一下当前这个锅的位置，如果还是没标签，走正常逻辑，如果有标签，就添加标签，换生成物
-                local x, y, z = self.inst.Transform:GetWorldPosition()
-                local roomtype = TheWorld.components.roomsystem:GetRoomTypeByWorldPos(x, z)
-
-                if roomtype == "kitchen" then
-                    --便携香料站不与厨房系统互动，但是带有厨房加成的料理加香料能产出带有厨房加成和香料的料理，具体参考kitchen_buff_foods.lua
-                    if self.inst:HasTag("spicer") then loot = SpawnPrefab(self.product) else loot = SpawnPrefab(self.product .. "_kitchen_buff") end
-
-                    --如果是一些不可食用或者合成表没的料理就返回原料理
-                    if not loot then loot = SpawnPrefab(self.product) end
-                    self.inst:AddTag("in_kitchen")
-                else
-                    loot = SpawnPrefab(self.product)
-                end
-            end
-
-
-            if loot ~= nil then
-				local recipe = cooking.GetRecipe(self.inst.prefab, self.product)
-
-				if harvester ~= nil and
-					self.chef_id == harvester.userid and
-					recipe ~= nil and
-					recipe.cookbook_category ~= nil and
-					cooking.cookbook_recipes[recipe.cookbook_category] ~= nil and
-					cooking.cookbook_recipes[recipe.cookbook_category][self.product] ~= nil then
-					harvester:PushEvent("learncookbookrecipe", {product = self.product, ingredients = self.ingredient_prefabs})
-				end
-
-				local stacksize = recipe and recipe.stacksize or 1
-				if stacksize > 1 then
-					loot.components.stackable:SetStackSize(stacksize)
-				end
-
-                if self.spoiltime ~= nil and loot.components.perishable ~= nil then
-                    local spoilpercent = self:GetTimeToSpoil() / self.spoiltime
-                    loot.components.perishable:SetPercent(self.product_spoilage * spoilpercent)
-                    loot.components.perishable:StartPerishing()
-                end
-                if harvester ~= nil and harvester.components.inventory ~= nil then
-                    harvester.components.inventory:GiveItem(loot, nil, self.inst:GetPosition())
-                else
-                    LaunchAt(loot, self.inst, nil, 1, 1)
-                end
-            end
-            self.product = nil
-        end
-
-        if self.task ~= nil then
-            self.task:Cancel()
-            self.task = nil
-        end
-        self.targettime = nil
-        self.done = nil
-        self.spoiltime = nil
-        self.product_spoilage = nil
-
-        if self.inst.components.container ~= nil then
-            self.inst.components.container.canbeopened = true
-        end
-
-        return true
-    end
-end
+--local function roomsysHarvest(self,harvester)
+--    if self.done then
+--        if self.onharvest ~= nil then
+--            self.onharvest(self.inst)
+--        end
+--
+--        if self.product ~= nil then
+--            local loot
+--
+--            --有标签，直接换生成物
+--            if self.inst:HasTag("in_kitchen") then
+--                --便携香料站不与厨房系统互动，但是带有厨房加成的料理加香料能产出带有厨房加成和香料的料理，具体参考kitchen_buff_foods.lua
+--                if self.inst:HasTag("spicer") then loot = SpawnPrefab(self.product) else loot = SpawnPrefab(self.product .. "_kitchen_buff") end
+--
+--                --如果是一些不可食用或者合成表没的料理就返回原料理
+--                if not loot then loot = SpawnPrefab(self.product) end
+--
+--            else--没标签，判断一下当前这个锅的位置，如果还是没标签，走正常逻辑，如果有标签，就添加标签，换生成物
+--                local x, y, z = self.inst.Transform:GetWorldPosition()
+--                local roomtype = TheWorld.components.roomsystem:GetRoomTypeByWorldPos(x, z)
+--
+--                if roomtype == "kitchen" then
+--                    --便携香料站不与厨房系统互动，但是带有厨房加成的料理加香料能产出带有厨房加成和香料的料理，具体参考kitchen_buff_foods.lua
+--                    if self.inst:HasTag("spicer") then loot = SpawnPrefab(self.product) else loot = SpawnPrefab(self.product .. "_kitchen_buff") end
+--
+--                    --如果是一些不可食用或者合成表没的料理就返回原料理
+--                    if not loot then loot = SpawnPrefab(self.product) end
+--                    self.inst:AddTag("in_kitchen")
+--                else
+--                    loot = SpawnPrefab(self.product)
+--                end
+--            end
+--
+--
+--            if loot ~= nil then
+--				local recipe = cooking.GetRecipe(self.inst.prefab, self.product)
+--
+--				if harvester ~= nil and
+--					self.chef_id == harvester.userid and
+--					recipe ~= nil and
+--					recipe.cookbook_category ~= nil and
+--					cooking.cookbook_recipes[recipe.cookbook_category] ~= nil and
+--					cooking.cookbook_recipes[recipe.cookbook_category][self.product] ~= nil then
+--					harvester:PushEvent("learncookbookrecipe", {product = self.product, ingredients = self.ingredient_prefabs})
+--				end
+--
+--				local stacksize = recipe and recipe.stacksize or 1
+--				if stacksize > 1 then
+--					loot.components.stackable:SetStackSize(stacksize)
+--				end
+--
+--                if self.spoiltime ~= nil and loot.components.perishable ~= nil then
+--                    local spoilpercent = self:GetTimeToSpoil() / self.spoiltime
+--                    loot.components.perishable:SetPercent(self.product_spoilage * spoilpercent)
+--                    loot.components.perishable:StartPerishing()
+--                end
+--                if harvester ~= nil and harvester.components.inventory ~= nil then
+--                    harvester.components.inventory:GiveItem(loot, nil, self.inst:GetPosition())
+--                else
+--                    LaunchAt(loot, self.inst, nil, 1, 1)
+--                end
+--            end
+--            self.product = nil
+--        end
+--
+--        if self.task ~= nil then
+--            self.task:Cancel()
+--            self.task = nil
+--        end
+--        self.targettime = nil
+--        self.done = nil
+--        self.spoiltime = nil
+--        self.product_spoilage = nil
+--
+--        if self.inst.components.container ~= nil then
+--            self.inst.components.container.canbeopened = true
+--        end
+--
+--        return true
+--    end
+--end
 --local function dump(o, indent)
 --    indent = indent or 0
 --    local prefix = string.rep("  ", indent)
@@ -104,13 +103,53 @@ end
 --    end
 --    print(prefix .. "}")
 --end
+local function SpawnFood(Stewer)
+    --Stewer.inst：有stewer组件的那个实例，比如烹饪锅
+    local loot
+
+    --有厨房标签
+    if Stewer.inst:HasTag("in_kitchen") then
+        --便携香料站不与厨房系统互动，但是带有厨房加成的料理加香料能产出带有厨房加成和香料的料理，具体参考kitchen_buff_foods.lua
+        if Stewer.inst:HasTag("spicer") then loot = SpawnPrefab(Stewer.product) else loot = SpawnPrefab(Stewer.product .. "_kitchen_buff") end
+
+        --如果是一些不可食用或者合成表没的料理就返回原料理
+        if not loot then loot = SpawnPrefab(Stewer.product) end
+
+    else--没厨房标签，判断一下当前这个锅的位置，如果还是没标签，走正常逻辑，如果有标签，就添加标签，换生成物
+
+        local x, y, z = Stewer.inst.Transform:GetWorldPosition()
+        local roomtype = TheWorld.components.roomsystem:GetRoomTypeByWorldPos(x, z)
+
+        if roomtype == "kitchen" then
+            --便携香料站不与厨房系统互动，但是带有厨房加成的料理加香料能产出带有厨房加成和香料的料理，具体参考kitchen_buff_foods.lua
+            if Stewer.inst:HasTag("spicer") then loot = SpawnPrefab(Stewer.product) else loot = SpawnPrefab(Stewer.product .. "_kitchen_buff") end
+
+            --如果是一些不可食用或者合成表没的料理就返回原料理
+            if not loot then loot = SpawnPrefab(Stewer.product) end
+            Stewer.inst:AddTag("in_kitchen")
+        else
+            loot = SpawnPrefab(Stewer.product)
+        end
+    end
+
+    return loot
+end
+
 AddComponentPostInit("stewer",function(Stewer)
+    local oldHarvest = Stewer.Harvest
     function Stewer:Harvest(harvester)
         if self.inst:HasTag("in_kitchen") then
             print("this cookpot in kitchen !")
         end
-        return roomsysHarvest(self,harvester)
-    end
+        --老套路，整个假的SpawnPrefab给他然后再还原
+        local oldSpawnPrefab = SpawnPrefab
+        local product = SpawnFood(Stewer)
+        local function fake_spawnprefab(_)  return product end
+        SpawnPrefab = fake_spawnprefab
+        local result = oldHarvest(self,harvester)
+        SpawnPrefab = oldSpawnPrefab
+        return result
+        end
 
     local oldStartCooking = Stewer.StartCooking
     function Stewer:StartCooking(doer)
